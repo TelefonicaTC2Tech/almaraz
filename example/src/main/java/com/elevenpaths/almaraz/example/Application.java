18fe7fa5ecc56ff4b48360e0bff36d8c81b3926a
mlopezg2 <mlopezg2@gmail.com>
2020-03-13 09:29:14 +0100

    fix: feedback received

--- example/src/main/java/com/elevenpaths/almaraz/example/Application.java
+++ example/src/main/java/com/elevenpaths/almaraz/example/Application.java
@@ -7,7 +7,7 @@ package com.elevenpaths.almaraz.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.elevenpaths.almaraz" })
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
